5b05d02de97c824f2cff12e64f127621f8c67f7b
Caresle <carabesle@gmail.com>
2025-03-30 15:35:04 -0600

    feat: add columns of items section

---
+++ admin-web/src/app/items/columns.tsx
@@ -0,0 +1,50 @@
+import Icons from "@/components/shared/icons"
+import { Button } from "@/components/ui/button"
+import { User } from "@/entities/user.entity"
+import { ColumnDef, createColumnHelper } from "@tanstack/react-table"
+import { useItemStore } from "./_states/item.state"
+import { useItemDeleteStore } from "./_states/item-delete.state"
+
+const columnsHelper = createColumnHelper<User & { actions: Object }>()
+
+export const useColumns = () => {
+	const { update } = useItemStore(state => state)
+	const { update: updateDelete } = useItemDeleteStore(state => state)
+
+	return [
+		columnsHelper.accessor("name", {
+			header: () => (
+				<div>
+					<Button variant={"secondary"} className="w-full">
+						Name
+					</Button>
+				</div>
+			),
+		}),
+		columnsHelper.accessor("actions", {
+			header: () => <></>,
+			cell: ({ row }) => {
+				return (
+					<div className="flex justify-end w-fit gap-2">
+						<Button
+							size={"icon"}
+							onClick={() =>
+								update({ isEdit: true, item: row.original, show: true })
+							}
+						>
+							<Icons.Actions.Edit />
+						</Button>
+						<Button
+							size={"icon"}
+							variant={"destructive"}
+							onClick={() => updateDelete({ item: row.original, show: true })}
+						>
+							<Icons.Actions.Delete />
+						</Button>
+					</div>
+				)
+			},
+			size: 15,
+		}),
+	] as ColumnDef<User>[]
+}
