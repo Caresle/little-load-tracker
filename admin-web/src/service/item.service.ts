01b3f38df1c62ea2048da5c010a9b3a3a18e3846
Caresle <carabesle@gmail.com>
2025-03-30 15:34:41 -0600

    feat: add items service

---
+++ admin-web/src/service/item.service.ts
@@ -0,0 +1,62 @@
+import { Item } from "@/entities/item.entity"
+import { axiosInstance } from "@/lib/axios"
+
+class ItemServiceDefinition {
+	version: string
+
+	constructor(version: string) {
+		this.version = version
+	}
+
+	setVersion(version: string) {
+		this.version = version
+	}
+
+	async getAll(): Promise<Item[]> {
+		try {
+			const res = await axiosInstance.get(`/${this.version}/items`)
+			return res.data
+		} catch (error) {
+			console.error(error)
+			return []
+		}
+	}
+
+	async create(item: Item): Promise<string> {
+		try {
+			const res = await axiosInstance.post(`/${this.version}/items`, item)
+			return res.data
+		} catch (error) {
+			console.error(error)
+			return "error"
+		}
+	}
+
+	async update(item: Item): Promise<string> {
+		try {
+			const res = await axiosInstance.put(
+				`/${this.version}/users/${item.id}`,
+				item
+			)
+			return res.data
+		} catch (error) {
+			console.error(error)
+			return "error"
+		}
+	}
+
+	async deleteOne(item: Item): Promise<string> {
+		try {
+			const res = await axiosInstance.delete(
+				`/${this.version}/items/${item.id}`
+			)
+			return res.data
+		} catch (error) {
+			console.error(error)
+			return "error"
+		}
+	}
+}
+
+const ItemService = new ItemServiceDefinition("v1")
+export default ItemService
