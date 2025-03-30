dba73ae3d03a4b99b940c065fc5d45a324c632a3
Caresle <carabesle@gmail.com>
2025-03-30 16:20:40 -0600

    feat: add items provider in items page

---
+++ admin-web/src/app/items/client.tsx
@@ -0,0 +1,55 @@
+"use client"
+import React from "react"
+import ItemsTable from "./_components/items-table"
+import { useItemStore } from "./_states/item.state"
+import { Button } from "@/components/ui/button"
+import Icons from "@/components/shared/icons"
+import { Item } from "@/entities/item.entity"
+import ItemModal from "./_components/_modals/item-modal"
+import ItemDeleteModal from "./_components/_modals/item-delete-modal"
+import { ItemsProvider, useItems } from "./_hooks/use-items"
+import Loading from "./loading"
+import NoItems from "./_components/no-items"
+
+const ItemActions = () => {
+	const { update } = useItemStore(state => state)
+	return (
+		<div className="flex items-end w-full bg-white rounded-lg p-2 border">
+			<Button
+				onClick={() => update({ show: true, isEdit: false, item: {} as Item })}
+			>
+				<Icons.Actions.Add />
+				Add Item
+			</Button>
+		</div>
+	)
+}
+
+const ItemsContent = () => {
+	const { items, QItems } = useItems()
+
+	return (
+		<div className="bg-white rounded-lg flex-1 border flex flex-col p-2 gap-1 overflow-y-auto">
+			{QItems?.isLoading && <Loading />}
+			{!QItems?.isLoading && items?.length <= 0 && <NoItems />}
+			{!QItems?.isLoading && items?.length > 0 && <ItemsTable />}
+		</div>
+	)
+}
+
+export default function ItemsClient({
+	intiialItems = [],
+}: {
+	intiialItems: Item[]
+}) {
+	return (
+		<ItemsProvider initialItems={intiialItems}>
+			<div className="w-full flex flex-col gap-2 p-2 overflow-y-auto">
+				<ItemModal />
+				<ItemDeleteModal />
+				<ItemActions />
+				<ItemsContent />
+			</div>
+		</ItemsProvider>
+	)
+}
--- admin-web/src/app/items/page.tsx
+++ admin-web/src/app/items/page.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import ItemsClient from "./client"
+import getItems from "@/actions/items/get-items"
 
-export default function ItemsPage() {
-	return <ItemsClient />
+export default async function ItemsPage() {
+	const items = await getItems()
+
+	return <ItemsClient intiialItems={items} />
 }
