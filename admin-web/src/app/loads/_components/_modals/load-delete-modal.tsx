740030f2aaa63ee2bce56846c9c2816e5616094f
Caresle <carabesle@gmail.com>
2025-04-06 14:33:35 -0600

    feat: add integration of delete modal

--- admin-web/src/app/loads/_components/_modals/load-delete-modal.tsx
+++ admin-web/src/app/loads/_components/_modals/load-delete-modal.tsx
@@ -12,11 +12,28 @@ import React from "react"
 import Icons from "@/components/shared/icons"
 import { Input } from "@/components/ui/input"
 import { useLoadDeleteStore } from "../../_states/load-delete.state"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import LoadService from "@/service/load.service"
+import { Load } from "@/entities/load.entity"
+import { queryKeys } from "@/constants/queryKeys"
 
 export default function LoadDeleteModal() {
 	const { show, update, load } = useLoadDeleteStore(state => state)
+	const queryClient = useQueryClient()
 
-	const onSubmit = () => {}
+	const mut = useMutation({
+		mutationFn: () => LoadService.deleteOne(load!),
+		onSuccess: () => {
+			update({ show: false, load: {} as Load })
+			queryClient.invalidateQueries({
+				queryKey: [queryKeys.loads],
+			})
+		},
+	})
+
+	const onSubmit = () => {
+		mut.mutate()
+	}
 
 	return (
 		<Dialog open={show} onOpenChange={value => update({ show: value })}>
@@ -28,14 +45,19 @@ export default function LoadDeleteModal() {
 					</DialogTitle>
 					<DialogDescription>Manage the data of the load</DialogDescription>
 				</DialogHeader>
-				<form className="flex flex-col gap-2">
-					<Input placeholder="Name" />
-				</form>
+				<p>
+					Are you sure you want to delete the load <strong>{load?.name}</strong>
+					?
+				</p>
 				<DialogFooter>
 					<DialogClose asChild>
 						<Button variant={"secondary"}>Cancel</Button>
 					</DialogClose>
-					<Button onClick={onSubmit} variant={"destructive"}>
+					<Button
+						onClick={onSubmit}
+						variant={"destructive"}
+						disabled={mut.isPending}
+					>
 						Confirm
 					</Button>
 				</DialogFooter>
