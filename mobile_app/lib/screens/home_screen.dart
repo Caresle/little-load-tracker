6bd8870b0744579c8a1ba52736d5226bdb6acf10
Caresle <carabesle@gmail.com>
2025-04-10 20:34:02 -0600

    feat: add basic design of home screen

--- mobile_app/lib/screens/home_screen.dart
+++ mobile_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/config/theme/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,11 +23,23 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             ListTile(
-              title: Text('Loads'),
+              title: Row(
+                children: [
+                  Icon(Icons.fire_truck_rounded),
+                  const SizedBox(width: 8),
+                  Text('Loads'),
+                ],
+              ),
               onTap: () {},
             ),
             ListTile(
-              title: Text('Settings'),
+              title: Row(
+                children: [
+                  Icon(Icons.settings_rounded),
+                  const SizedBox(width: 8),
+                  Text('Settings'),
+                ],
+              ),
               onTap: () {},
             ),
           ],
@@ -42,9 +55,97 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: SafeArea(
-          child: Center(
-        child: Text('Home screen'),
-      )),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Recent Loads'),
+                const SizedBox(height: 8),
+                _LoadCard(),
+                const SizedBox(height: 8),
+                _LoadCard(),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue.shade800,
+        foregroundColor: Colors.white,
+        onPressed: () {},
+        shape: CircleBorder(),
+        tooltip: 'Scan QR LOAD',
+        child: Icon(Icons.qr_code_2_rounded),
+      ),
+    );
+  }
+}
+
+class _LoadCard extends StatelessWidget {
+  const _LoadCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.grey.shade400),
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.fire_truck_rounded),
+                    const SizedBox(width: 4),
+                    Text('1 - Load Test'),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_horiz_rounded),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Chip(
+                  label: Text('chip'),
+                ),
+              ],
+            ),
+            Text('Description'),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton.tonal(
+                style: buttonSecondaryTonal,
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.remove_red_eye_rounded,
+                      color: Colors.orange.shade900,
+                    ),
+                    const SizedBox(width: 8),
+                    Text('View Load'),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
