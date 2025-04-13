39c826234677c7bd7fd090cc66130b82e35cc089
Caresle <carabesle@gmail.com>
2025-04-13 05:52:33 -0600

    feat: add hasAccess to [load] route

--- admin-web/src/app/api/v1/loads/[load]/route.ts
+++ admin-web/src/app/api/v1/loads/[load]/route.ts
@@ -1,62 +1,20 @@
-import { pgQuery } from "@/lib/pg"
+import {
+	GET as getLoad,
+	PUT as updateLoad,
+	DELETE as deleteLoad,
+} from "./controller"
+
+import { hasAccess } from "@/helpers/has-access"
+import { PERMISSIONS } from "@/constants/permissions"
 import { NextRequest } from "next/server"
-import { QueriesLoads } from "../queries"
-import { loadsUpdateSchema } from "../schema"
-import getLoadById from "@/actions/load/get-load-by-id"
 
-export async function GET(
-	_: NextRequest,
-	{ params }: { params: Promise<{ load: string }> }
-) {
-	try {
-		const id = (await params).load
-		const load = await getLoadById(+id)
-		return Response.json(load, { status: 200 })
-	} catch (error) {
-		console.error(error)
-		return Response.json({ error: "Something went wrong" }, { status: 500 })
-	}
-}
+const GET = (req: NextRequest, params: any) =>
+	hasAccess(PERMISSIONS.loads.getLoads, req, params, getLoad)
 
-export async function PUT(
-	req: NextRequest,
-	{ params }: { params: Promise<{ load: string }> }
-) {
-	try {
-		const load = (await params).load
-		const json = await req.json()
-		const validate = loadsUpdateSchema.parse({ ...json, id: load })
+const PUT = (req: NextRequest, params: any) =>
+	hasAccess(PERMISSIONS.loads.updateLoad, req, params, updateLoad)
 
-		await pgQuery(QueriesLoads.updateHeader, [
-			validate.name,
-			validate.description,
-			validate.loadType,
-			validate.loadStatus,
-			load,
-		])
+const DELETE = (req: NextRequest, params: any) =>
+	hasAccess(PERMISSIONS.loads.deleteLoad, req, params, deleteLoad)
 
-		await pgQuery(QueriesLoads.createDetails, [
-			load,
-			{ items: validate.details },
-		])
-
-		return Response.json("updated", { status: 200 })
-	} catch (error) {
-		console.error(error)
-		return Response.json({ error: "Something went wrong" }, { status: 500 })
-	}
-}
-
-export async function DELETE(
-	_: NextRequest,
-	{ params }: { params: Promise<{ load: string }> }
-) {
-	try {
-		const load = (await params).load
-		await pgQuery(QueriesLoads.deleteLoad, [load])
-		return Response.json("ok", { status: 200 })
-	} catch (error) {
-		console.error(error)
-		return Response.json({ error: "Something went wrong" }, { status: 500 })
-	}
-}
+export { GET, PUT, DELETE }
