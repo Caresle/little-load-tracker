bc516b47ccaf1358eaf02eff7bc3c8e3139933a9
Carlos Estrada <carabesle@gmail.com>
2025-04-20 11:05:04 -0600

    feat: add redirect to login
    In case token is not present in the localstorage, using a provider for
    this

---
+++ admin-web/src/providers/has-token-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useRouter } from "next/navigation"
+import React, { useEffect } from "react"
+
+export default function HasTokenProvider({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const router = useRouter()
+
+	useEffect(() => {
+		const token = window.localStorage.getItem("token")
+		if (!token) {
+			router.push("/login")
+		}
+	}, [])
+
+	return <div>{children}</div>
+}
--- admin-web/src/providers/providers.tsx
+++ admin-web/src/providers/providers.tsx
@@ -3,13 +3,17 @@ import React from "react"
 import TanstackProvider from "./tanstack-provider"
 import { Toaster } from "sonner"
 import { ThemeProvider } from "./theme-provider"
+import HasTokenProvider from "./has-token-provider"
+
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
-				<Toaster richColors expand closeButton position="top-right" />
-				<TanstackProvider>{children}</TanstackProvider>
-			</ThemeProvider>
+			<HasTokenProvider>
+				<ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
+					<Toaster richColors expand closeButton position="top-right" />
+					<TanstackProvider>{children}</TanstackProvider>
+				</ThemeProvider>
+			</HasTokenProvider>
 		</>
 	)
 }
