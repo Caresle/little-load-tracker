57016fa9480d7212847dd528b065059b7438fae7
Carlos Estrada <carabesle@gmail.com>
2025-04-22 20:49:50 -0600

    feat: add axios response interceptor

--- admin-web/src/lib/axios.ts
+++ admin-web/src/lib/axios.ts
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 
 const instance = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -19,7 +19,18 @@ export const tokenInterceptor = instance.interceptors.request.use(
 	},
 	function (error) {
 		return Promise.reject(error)
-	}
+	},
+)
+
+export const responseInterceptor = instance.interceptors.response.use(
+	response => response,
+	error => {
+		const originalRequest = error.config?.url
+		if (error?.response?.status === 401 && originalRequest !== "/login") {
+			window.location.href = "/login"
+		}
+		return Promise.reject(error)
+	},
 )
 
 export const axiosInstance = instance
