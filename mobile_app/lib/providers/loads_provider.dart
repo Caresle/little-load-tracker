a41b80604e56e617a70bd09f7290f8d03d731842
Carlos Estrada <carabesle@gmail.com>
2025-04-29 19:43:46 -0600

    feat: add loads provider methods for handle ui logic

--- mobile_app/lib/providers/loads_provider.dart
+++ mobile_app/lib/providers/loads_provider.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/entities/load.dart';
+import 'package:mobile_app/services/loads_service.dart';
 
 class LoadsProvider extends ChangeNotifier {
   List<Load> loads = [];
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
 
   void setLoads(List<Load> loads) {
     this.loads = loads;
@@ -17,5 +21,19 @@ class LoadsProvider extends ChangeNotifier {
   void removeLoad(Load load) {
     // loads.remove(load);
     notifyListeners();
+  }
+
+  Future<void> getLoads() async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      final loads = await LoadsService.getLoads();
+      this.loads = loads;
+    } catch (e) {
+      print(e);
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
   }
 }
