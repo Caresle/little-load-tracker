b2fc5ddf101c73dd5705f04b6dd3b4981847db03
Carlos Estrada <carabesle@gmail.com>
2025-04-30 20:31:20 -0600

    refactor(mobile-app): app drawer

--- mobile_app/lib/widgets/shared/app_drawer.dart
+++ mobile_app/lib/widgets/shared/app_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/constants/token.dart';
+import 'package:mobile_app/helpers/jwt.dart';
+import 'package:mobile_app/helpers/shared_config.dart';
 import 'package:mobile_app/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -7,49 +10,47 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = context.read<ThemeProvider>();
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue.shade600,
-            ),
-            child: Text(
-              'Little Load Tracker',
-              style: TextStyle(
-                color: Colors.white,
+          SizedBox(
+            width: double.infinity,
+            child: DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue.shade600),
+              child: Text(
+                'Little Load Tracker',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
           ),
           ListTile(
-            title: Row(
-              children: [
-                Icon(Icons.fire_truck_rounded),
-                const SizedBox(width: 8),
-                Text('Loads'),
-              ],
-            ),
-            onTap: () {},
-          ),
-          ListTile(
-            title: Row(
-              children: [
-                Icon(Icons.settings_rounded),
-                const SizedBox(width: 8),
-                Text('Settings'),
-              ],
-            ),
-            onTap: () {
-              theme.toggleTheme();
+            leading: Icon(Icons.fire_truck_rounded),
+            title: Text('Loads'),
+            onTap: () async {
+              print('loads tapped');
+              final token = await SharedConfig.get(TokenConstants.key);
+              if (token == null) {
+                print('No token found');
+                return;
+              }
+
+              verifyJwt(token);
             },
           ),
-          SwitchListTile(
-            subtitle: Text('Dark Mode'),
-            value: theme.isDark ? true : false,
-            onChanged: (_) => theme.toggleTheme(),
-          )
+          ListTile(
+            leading: Icon(Icons.settings_rounded),
+            title: Text('Settings'),
+            onTap: () => context.read<ThemeProvider>().toggleTheme(),
+          ),
+          Spacer(), // Pushes the switch to the bottom
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: SwitchListTile(
+              title: Text('Dark Mode'),
+              value: context.watch<ThemeProvider>().isDark,
+              onChanged: (_) => context.read<ThemeProvider>().toggleTheme(),
+            ),
+          ),
         ],
       ),
     );
